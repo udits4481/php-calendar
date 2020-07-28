6da16e7f707d5397c53c2c85fea7c376428c8d40
udits4481 <68885302+udits4481@users.noreply.github.com>
2020-07-28 10:08:23 +0530

    Add files via upload

---
+++ book.php
@@ -0,0 +1,75 @@
+<?php
+if(isset($_GET['date'])){
+    $date = $_GET['date'];
+}
+
+if(isset($_POST['submit'])){
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $mysqli = new mysqli('localhost', 'root', '', 'bookingcalendar');
+    $stmt = $mysqli->prepare("INSERT INTO bookings (name, email, date) VALUES (?,?,?)");
+    $stmt->bind_param('sss', $name, $email, $date);
+    $stmt->execute();
+    $msg = "<div class='alert alert-success'>Booking Successfull</div>";
+    $stmt->close();
+    $mysqli->close();
+}
+
+$duration=60;
+$cleanup=0;
+$start="09:00";
+$end="15:00";
+
+function timeslots($duration, $cleanup, $start, $end){
+    $start=new DateTime($start);
+    $end=new DateTime($end);
+    $interval=new Dateinterval("PT".$duration."M");
+    $cleanupInterval=new DateInterval("PT".&cleanup."M");
+    $slots=array();
+    
+    for($intStart=$start;$intStart<$end;$intStart->add($interval)->add($cleanupInterval)){
+        $endPeriod = clone $intStart;
+        $endPeriod->add($interval);
+        if($endPeriod>$end)
+        {
+            break;
+        }
+        $slots[]=$intStart->format("H:iA")." - ".$endPeriod->frmat("H:iA");
+    }
+       return $slots;
+}
+
+?>
+<!doctype html>
+<html lang="en">
+
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title></title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="/css/main.css">
+  </head>
+
+  <body>
+    <div class="container">
+        <h1 class="text-center">Book for Date: <?php echo date('m/d/Y', strtotime($date)); ?></h1><hr>
+        <div class="row">
+            <?php $timeslots = timeslots($duration,$cleanup,$start,$end);
+            foreach($timeslots as $ts){
+                ?>
+            <div class="col-md-2">
+            <button class="btn btn-success"><?php echo $ts ?></button>
+            </div>
+            <?php}?>
+        </div>
+    </div>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+  </body>
+
+</html>
